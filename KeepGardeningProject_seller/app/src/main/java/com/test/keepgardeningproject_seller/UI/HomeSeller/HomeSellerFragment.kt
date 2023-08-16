5acf44628fd67a52101e0069d8e0883ad6b93cf3
PIYUJIN <yjyj020201@gmail.com>
2023-08-17 02:00:20 +0900

    [Feat] HomeSellerFragment.kt 파일 기능 구현
    HomeSellerFragment.kt 파일에 bottomNavigation menu 선택시 기능 분기

--- KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/HomeSeller/HomeSellerFragment.kt
+++ KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/HomeSeller/HomeSellerFragment.kt
@@ -1,14 +1,24 @@
 package com.test.keepgardeningproject_seller.UI.HomeSeller
 
+import android.content.DialogInterface
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.test.keepgardeningproject_seller.MainActivity
+import com.test.keepgardeningproject_seller.MainActivity.Companion.AUCTION_SELLER_REGISTER_FRAGMENT
+import com.test.keepgardeningproject_seller.MainActivity.Companion.PRODUCT_SELLER_REGISTER_FRAGMENT
 import com.test.keepgardeningproject_seller.R
+import com.test.keepgardeningproject_seller.databinding.FragmentHomeSellerBinding
 
 class HomeSellerFragment : Fragment() {
+
+    lateinit var fragmentHomeSellerBinding: FragmentHomeSellerBinding
+    lateinit var mainActivity: MainActivity
+
 
     companion object {
         fun newInstance() = HomeSellerFragment()
@@ -20,7 +30,27 @@ class HomeSellerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home_seller, container, false)
+        fragmentHomeSellerBinding = FragmentHomeSellerBinding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+
+        fragmentHomeSellerBinding.run {
+            bottomNavigationViewHomeSeller.run {
+                selectedItemId = R.id.item_bottomMenu_home
+                setOnItemSelectedListener {
+                    when(it.itemId) {
+                        R.id.item_bottomMenu_home -> {
+                        }
+                        R.id.item_bottomMenu_registerProduct -> {
+                        }
+                        R.id.item_bottomMenu_mypage -> {
+                        }
+                    }
+                    true
+                }
+            }
+        }
+
+        return fragmentHomeSellerBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -28,5 +58,4 @@ class HomeSellerFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(HomeSellerViewModel::class.java)
         // TODO: Use the ViewModel
     }
-
 }
