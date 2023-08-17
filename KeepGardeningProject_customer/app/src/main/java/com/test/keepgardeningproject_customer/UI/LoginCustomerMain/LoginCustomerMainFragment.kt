ab1a95c9b9d9449fc7bdd4c1f8a2caec953d050e
dkin12 <dkin1020@naver.com>
2023-08-17 13:32:19 +0900

    [design] 구매자 로그인, 회원가입 버튼 연결 및 splishIcon 설정완료
    mainAcitivty -> splish 설정
    manifest.xml -> splish 설정
    
    kt파일에서 이미지 넘기고 email 형식 설정

--- KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/LoginCustomerMain/LoginCustomerMainFragment.kt
+++ KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/LoginCustomerMain/LoginCustomerMainFragment.kt
@@ -6,27 +6,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.test.keepgardeningproject_customer.MainActivity
 import com.test.keepgardeningproject_customer.R
+import com.test.keepgardeningproject_customer.databinding.FragmentLoginCustomerMainBinding
 
 class LoginCustomerMainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = LoginCustomerMainFragment()
-    }
-
-    private lateinit var viewModel: LoginCustomerMainViewModel
-
+    lateinit var fragmentLoginCustomerMainBinding:FragmentLoginCustomerMainBinding
+    lateinit var mainActivity: MainActivity
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login_customer_main, container, false)
+        fragmentLoginCustomerMainBinding = FragmentLoginCustomerMainBinding.inflate(inflater)
+        mainActivity = activity as  MainActivity
+
+        fragmentLoginCustomerMainBinding.run {
+            toolbarLoginCustomerMain.run {
+                setTitle("로그인")
+                setNavigationIcon(R.drawable.ic_back_24px)
+                setNavigationOnClickListener {
+                    mainActivity.removeFragment(MainActivity.LOGIN_CUSTOMER_MAIN_FRAGMENT)
+                }
+            }
+            // 카카오 로그인
+            buttonLoginCustomerMainKakaoLogin.setOnClickListener {
+                Toast.makeText(requireContext(),"kakao",Toast.LENGTH_SHORT).show()
+            }
+            // 네이버 로그인
+            buttonLoginCustomerMainNaverLogin.setOnClickListener {
+                Toast.makeText(requireContext(),"naver",Toast.LENGTH_SHORT).show()
+            }
+            // 구글 로그인
+            buttonLoginCustomerMainGoogleLogin.setOnClickListener {
+                Toast.makeText(requireContext(),"google",Toast.LENGTH_SHORT).show()
+            }
+            // 이메일 로그인
+            buttonLoginCustomerMainEmailLogin.setOnClickListener {
+                mainActivity.replaceFragment(MainActivity.LOGIN_CUSTOMER_TO_EMAIL_FRAGMENT,true,null)
+            }
+        }
+
+        return fragmentLoginCustomerMainBinding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LoginCustomerMainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
 }
