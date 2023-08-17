e2aa168d2de8148e648e934f8968a567a76625fe
PIYUJIN <yjyj020201@gmail.com>
2023-08-18 08:35:16 +0900

    [Design] AuctionSellerMainFragment.kt 파일 수정
    AuctionSellerMainFragment.kt 파일에 tab 구성

--- KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/AuctionSellerMain/AuctionSellerMainFragment.kt
+++ KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/AuctionSellerMain/AuctionSellerMainFragment.kt
@@ -6,9 +6,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.test.keepgardeningproject_seller.MainActivity
+import com.test.keepgardeningproject_seller.MainActivity.Companion.AUCTION_SELLER_EDIT_FRAGMENT
 import com.test.keepgardeningproject_seller.R
+import com.test.keepgardeningproject_seller.UI.AuctionSellerDetail.AuctionSellerDetailFragment
+import com.test.keepgardeningproject_seller.UI.AuctionSellerInfo.AuctionSellerInfoFragment
+import com.test.keepgardeningproject_seller.UI.AuctionSellerQnA.AuctionSellerQnAFragment
+import com.test.keepgardeningproject_seller.UI.ProductSellerDetail.ProductSellerDetailFragment
+import com.test.keepgardeningproject_seller.UI.ProductSellerQnA.ProductSellerQnAFragment
+import com.test.keepgardeningproject_seller.UI.ProductSellerReview.ProductSellerReviewFragment
+import com.test.keepgardeningproject_seller.databinding.FragmentAuctionSellerMainBinding
 
 class AuctionSellerMainFragment : Fragment() {
+
+    lateinit var fragmentAuctionSellerMainBinding: FragmentAuctionSellerMainBinding
+    lateinit var mainActivity: MainActivity
+
+    val fragmentList = mutableListOf<Fragment>()
+
+    // 탭에 표시할 이름
+    val tabName = arrayOf(
+        "상세정보","경매정보","문의"
+    )
 
     companion object {
         fun newInstance() = AuctionSellerMainFragment()
@@ -20,13 +43,46 @@ class AuctionSellerMainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_auction_seller_main, container, false)
+
+        fragmentAuctionSellerMainBinding = FragmentAuctionSellerMainBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+
+        fragmentList.add(AuctionSellerDetailFragment())
+        fragmentList.add(AuctionSellerInfoFragment())
+        fragmentList.add(AuctionSellerQnAFragment())
+
+        fragmentAuctionSellerMainBinding.run {
+
+            viewPagerAuctionSellerMainFragment.adapter = TabAdapterClass(mainActivity)
+
+                // 탭 구성
+                val tabLayoutMediator = TabLayoutMediator(tabAuctionSellerMain, viewPagerAuctionSellerMainFragment){ tab: TabLayout.Tab, i: Int ->
+                    tab.text = tabName[i]
+                }
+                tabLayoutMediator.attach()
+
+
+        }
+        return fragmentAuctionSellerMainBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(AuctionSellerMainViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    // adapter 클래스
+    inner class TabAdapterClass(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity){
+        override fun getItemCount(): Int {
+            return fragmentList.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragmentList[position]
+        }
+
     }
 
 }
