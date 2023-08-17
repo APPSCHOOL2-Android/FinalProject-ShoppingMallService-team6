8a935bfbedd70d18c34b4de39a463d8a5d2723c9
PIYUJIN <yjyj020201@gmail.com>
2023-08-18 08:15:04 +0900

    [Feat] ProductSellerEditFragment.kt 파일 기능 구현
    ProductSellerEditFragment.kt 파일에 bottomNavigation menu 선택시 기능 분기

--- KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/ProductSellerEdit/ProductSellerEditFragment.kt
+++ KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/ProductSellerEdit/ProductSellerEditFragment.kt
@@ -6,9 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.test.keepgardeningproject_seller.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.test.keepgardeningproject_seller.MainActivity
+import com.test.keepgardeningproject_seller.MainActivity.Companion.PRODUCT_SELLER_EDIT_FRAGMENT
+import com.test.keepgardeningproject_seller.databinding.FragmentProductSellerEditBinding
 
 class ProductSellerEditFragment : Fragment() {
+
+    lateinit var fragmentProductSellerEditBinding: FragmentProductSellerEditBinding
+    lateinit var mainActivity: MainActivity
 
     companion object {
         fun newInstance() = ProductSellerEditFragment()
@@ -20,7 +26,61 @@ class ProductSellerEditFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_product_seller_edit, container, false)
+
+        fragmentProductSellerEditBinding = FragmentProductSellerEditBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        fragmentProductSellerEditBinding.run {
+            val sheetBehavior = BottomSheetBehavior.from(includeProductSellerEdit.bottomSheetCategory)
+
+            sheetBehavior.isHideable = true
+            sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+            textInputEditTextProductSellerEditCategory.setOnTouchListener { v, event ->
+                sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                true
+            }
+
+            includeProductSellerEdit.run {
+                buttonCategoryBottomSheetPlant.setOnClickListener {
+                    textInputEditTextProductSellerEditCategory.setText("${buttonCategoryBottomSheetPlant.text}")
+                    sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                }
+                buttonCategoryBottomSheetOrchid.setOnClickListener {
+                    textInputEditTextProductSellerEditCategory.setText("${buttonCategoryBottomSheetOrchid.text}")
+                    sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                }
+                buttonCategoryBottomSheetMonstera.setOnClickListener {
+                    textInputEditTextProductSellerEditCategory.setText("${buttonCategoryBottomSheetMonstera.text}")
+                    sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                }
+                buttonCategoryBottomSheetBonsai.setOnClickListener {
+                    textInputEditTextProductSellerEditCategory.setText("${buttonCategoryBottomSheetBonsai.text}")
+                    sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                }
+                buttonCategoryBottomSheetCommodity.setOnClickListener {
+                    textInputEditTextProductSellerEditCategory.setText("${buttonCategoryBottomSheetCommodity.text}")
+                    sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                }
+                buttonCategoryBottomSheetSeed.setOnClickListener {
+                    textInputEditTextProductSellerEditCategory.setText("${buttonCategoryBottomSheetSeed.text}")
+                    sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                }
+                buttonCategoryBottomSheetBouquet.setOnClickListener {
+                    textInputEditTextProductSellerEditCategory.setText("${buttonCategoryBottomSheetBouquet.text}")
+                    sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                }
+                buttonCategoryBottomSheetPot.setOnClickListener {
+                    textInputEditTextProductSellerEditCategory.setText("${buttonCategoryBottomSheetPot.text}")
+                    sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                }
+            }
+
+            buttonProductSellerEditEdit.setOnClickListener {
+                mainActivity.removeFragment(PRODUCT_SELLER_EDIT_FRAGMENT)
+            }
+        }
+        return fragmentProductSellerEditBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
