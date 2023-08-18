21b0a1efd5a8635d68a350d3f9b0d36fc53ba839
PIYUJIN <yjyj020201@gmail.com>
2023-08-18 15:12:38 +0900

    [Fix] ProductSellerMainFragment.kt 파일 기능 수정
    ProductSellerMainFragment.kt 파일에 tab 구성 기능 수정

--- KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/ProductSellerMain/ProductSellerMainFragment.kt
+++ KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/ProductSellerMain/ProductSellerMainFragment.kt
@@ -9,11 +9,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.test.keepgardeningproject_seller.MainActivity
+import com.test.keepgardeningproject_seller.MainActivity.Companion.PRODUCT_SELLER_DETAIL_FRAGMENT
 import com.test.keepgardeningproject_seller.MainActivity.Companion.PRODUCT_SELLER_EDIT_FRAGMENT
+import com.test.keepgardeningproject_seller.MainActivity.Companion.PRODUCT_SELLER_QNA_FRAGMENT
+import com.test.keepgardeningproject_seller.MainActivity.Companion.PRODUCT_SELLER_REVIEW_FRAGMENT
 import com.test.keepgardeningproject_seller.R
 import com.test.keepgardeningproject_seller.UI.HomeSeller.HomeSellerAuctionFragment
 import com.test.keepgardeningproject_seller.UI.HomeSeller.HomeSellerProductFragment
@@ -53,30 +57,54 @@ class ProductSellerMainFragment : Fragment() {
         fragmentList.add(ProductSellerQnAFragment())
 
         fragmentProductSellerMainBinding.run {
-            viewPagerProductSellerMainFragment.adapter = TabAdapterClass(mainActivity)
 
-            // 탭 구성
-            val tabLayoutMediator = TabLayoutMediator(tabProductSellerMain, viewPagerProductSellerMainFragment){ tab: TabLayout.Tab, i: Int ->
-                tab.text = tabName[i]
+            tabLayoutProductSellerMain.run {
+                //ViewPager2 Adapter 셋팅
+                var viewPager2Adatper = ViewPager2Adapter(mainActivity)
+                viewPager2Adatper.addFragment(ProductSellerDetailFragment())
+                viewPager2Adatper.addFragment(ProductSellerReviewFragment())
+                viewPager2Adatper.addFragment(ProductSellerQnAFragment())
+
+                //Adapter 연결
+                fragmentProductSellerMainBinding.viewPagerProductSellerMainFragment.apply {
+                    adapter = viewPager2Adatper
+
+                    registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                        override fun onPageSelected(position: Int) {
+                            super.onPageSelected(position)
+                        }
+                    })
+                }
+
+                //ViewPager, TabLayout 연결
+                TabLayoutMediator(
+                    tabLayoutProductSellerMain,
+                    viewPagerProductSellerMainFragment
+                ) { tab, position ->
+                    when (position) {
+                        0 -> tab.text = "상세정보"
+                        1 -> tab.text = "리뷰"
+                        2 -> tab.text = "문의"
+                    }
+                }.attach()
+
+                buttonProductSellerMainEdit.setOnClickListener {
+                    mainActivity.replaceFragment(PRODUCT_SELLER_EDIT_FRAGMENT, true, null)
+                }
+
+                buttonProductSellerMainDelete.setOnClickListener {
+                    val builder = MaterialAlertDialogBuilder(mainActivity)
+                    builder.setMessage("해당 상품을 삭제하시겠습니까?")
+                    builder.setNegativeButton("취소", null)
+                    builder.setPositiveButton("삭제") { dialogInterface: DialogInterface, i: Int -> }
+                    builder.show()
+                }
+
             }
-            tabLayoutMediator.attach()
 
-            buttonProductSellerMainEdit.setOnClickListener {
-                mainActivity.replaceFragment(PRODUCT_SELLER_EDIT_FRAGMENT, true, null)
-            }
 
-            buttonProductSellerMainDelete.setOnClickListener {
-                val builder = MaterialAlertDialogBuilder(mainActivity)
-                builder.setMessage("해당 상품을 삭제하시겠습니까?")
-                builder.setNegativeButton("취소",null)
-                builder.setPositiveButton("삭제") { dialogInterface: DialogInterface, i: Int -> }
-                builder.show()
-            }
-
+            return fragmentProductSellerMainBinding.root
         }
-
-
-        return fragmentProductSellerMainBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -84,17 +112,29 @@ class ProductSellerMainFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(ProductSellerMainViewModel::class.java)
         // TODO: Use the ViewModel
     }
+}
 
-    // adapter 클래스
-    inner class TabAdapterClass(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity){
-        override fun getItemCount(): Int {
-            return fragmentList.size
-        }
+class ViewPager2Adapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+    var fragments: ArrayList<Fragment> = ArrayList()
 
-        override fun createFragment(position: Int): Fragment {
-            return fragmentList[position]
-        }
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
 
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+
+    fun addFragment(fragment: Fragment) {
+        fragments.add(fragment)
+        notifyItemInserted(fragments.size - 1)
+        //TODO: notifyItemInserted!!
+    }
+
+    fun removeFragement() {
+        fragments.removeLast()
+        notifyItemRemoved(fragments.size)
+        //TODO: notifyItemRemoved!!
     }
 
 }
