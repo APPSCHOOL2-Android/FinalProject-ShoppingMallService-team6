f39556dacacaafd3f62b658efa48792dd0a89da7
ShShin98 <hss980903@gmail.com>
2023-08-18 14:44:05 +0900

    [Design] [Feat] 스토어 상세 화면 UI, 화면 이동 처리
    - 스토어 상세화면에 상품목록 리사이클러뷰 구현
    - 상품목록에서 상품 클릭시 상품 상세페이지로 이동하도록 처리

--- KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/StoreInfoCustomerDetail/StoreInfoCustomerDetailFragment.kt
+++ KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/StoreInfoCustomerDetail/StoreInfoCustomerDetailFragment.kt
@@ -6,9 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.test.keepgardeningproject_customer.MainActivity
 import com.test.keepgardeningproject_customer.R
 import com.test.keepgardeningproject_customer.databinding.FragmentStoreInfoCustomerDetailBinding
+import com.test.keepgardeningproject_customer.databinding.RowHcsLinearBinding
+import com.test.keepgardeningproject_customer.databinding.RowStoreInfoCustomerBinding
 
 class StoreInfoCustomerDetailFragment : Fragment() {
     lateinit var fragmentStoreInfoCustomerDetailBinding: FragmentStoreInfoCustomerDetailBinding
@@ -31,8 +36,56 @@ class StoreInfoCustomerDetailFragment : Fragment() {
                     mainActivity.removeFragment(MainActivity.STORE_INFO_CUSTOMER_DETAIL_FRAGMENT)
                 }
             }
+
+            recyclerViewStoreInfoDetail.run {
+                adapter = StoreInfoDetailRecyclerViewAdpater()
+                layoutManager = LinearLayoutManager(context)
+            }
         }
 
         return fragmentStoreInfoCustomerDetailBinding.root
+    }
+
+    inner class StoreInfoDetailRecyclerViewAdpater : RecyclerView.Adapter<StoreInfoDetailRecyclerViewAdpater.StoreInfoDetailViewHolder>() {
+        inner class StoreInfoDetailViewHolder(rowHcsLinearBinding: RowHcsLinearBinding) :
+            RecyclerView.ViewHolder(rowHcsLinearBinding.root) {
+
+            var rowProductName: TextView
+            var rowStoreName: TextView
+            var rowPrice: TextView
+
+            init {
+                rowProductName = rowHcsLinearBinding.textViewHcsLinearTitle
+                rowStoreName = rowHcsLinearBinding.textViewHcsLinearStore
+                rowPrice = rowHcsLinearBinding.textViewHcsLinearPrice
+
+                rowHcsLinearBinding.root.setOnClickListener {
+                    mainActivity.replaceFragment(MainActivity.PRODUCT_CUSTOMER_DETAIL_FRAGMENT, true, null)
+                }
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoreInfoDetailViewHolder {
+            val rowHcsLinearBinding = RowHcsLinearBinding.inflate(layoutInflater)
+            val storeInfoDetailViewHolder = StoreInfoDetailViewHolder(rowHcsLinearBinding)
+
+            rowHcsLinearBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            return storeInfoDetailViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 10
+        }
+
+        override fun onBindViewHolder(holder: StoreInfoDetailViewHolder, position: Int) {
+            holder.rowProductName.text = "상품명"
+            holder.rowStoreName.text = "000스토어"
+            holder.rowPrice.text = "10,000원"
+
+        }
     }
 }
