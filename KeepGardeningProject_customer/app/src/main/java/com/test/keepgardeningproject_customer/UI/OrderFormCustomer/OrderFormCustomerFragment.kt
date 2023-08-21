528b7b7041d0c72d09c95c0fde9d48cd37ed9657
ShShin98 <hss980903@gmail.com>
2023-08-21 11:03:53 +0900

    [Feat] 사용자 앱 주문서 화면 유효성 검사 기능 추가
    주문서 화면에서 입력 정보들을 빠짐없이 입력해야 주문확인서로 넘어가도록 함

--- KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/OrderFormCustomer/OrderFormCustomerFragment.kt
+++ KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/OrderFormCustomer/OrderFormCustomerFragment.kt
@@ -1,19 +1,26 @@
 package com.test.keepgardeningproject_customer.UI.OrderFormCustomer
 
+import android.content.Context
+import android.content.DialogInterface
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.divider.MaterialDividerItemDecoration
 import com.test.keepgardeningproject_customer.MainActivity
 import com.test.keepgardeningproject_customer.R
 import com.test.keepgardeningproject_customer.databinding.FragmentOrderFormCustomerBinding
 import com.test.keepgardeningproject_customer.databinding.RowOrderFormCustomerBinding
+import kotlin.concurrent.thread
 
 class OrderFormCustomerFragment : Fragment() {
     lateinit var fragmentOrderFormCustomerBinding: FragmentOrderFormCustomerBinding
@@ -22,8 +29,7 @@ class OrderFormCustomerFragment : Fragment() {
     private lateinit var viewModel: OrderFormCustomerViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         fragmentOrderFormCustomerBinding = FragmentOrderFormCustomerBinding.inflate(inflater)
         mainActivity = activity as MainActivity
@@ -43,7 +49,8 @@ class OrderFormCustomerFragment : Fragment() {
             }
 
             buttonOrderFormSubmitOrder.setOnClickListener {
-                mainActivity.replaceFragment(MainActivity.ORDER_CHECK_FORM_CUSTOMER_FRAGMENT, true, null)
+                next()
+
             }
         }
 
@@ -62,8 +69,7 @@ class OrderFormCustomerFragment : Fragment() {
             val orderFormViewHolder = OrderFormViewHolder(rowOrderFormCustomerBinding)
 
             rowOrderFormCustomerBinding.root.layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
             )
 
             return orderFormViewHolder
@@ -78,5 +84,122 @@ class OrderFormCustomerFragment : Fragment() {
         }
     }
 
+    fun next() {
+        fragmentOrderFormCustomerBinding.run {
+            val ordererName = editTextOrderFormOrdererName.text.toString()
+            val ordererPhone = editTextOrderFormOrdererPhone.text.toString()
+            val ordererEmail = editTextOrderFormOrdererEmail.text.toString()
+            val receiverName = editTextOrderFormReceiverName.text.toString()
+            val addressPhone = editTextOrderFormAddressPhone.text.toString()
+            val zipCode = editTextOrderFormZipCode.text.toString()
+            val detailAddress = editTextOrderFormDetailAddress.text.toString()
+            val paymentMethod = radioGroupOrderFormPayment.checkedRadioButtonId
+            val paymentRadio1 = radioButtonOrderFormDeposit.isChecked
+            val paymentRadio2 = radioButtonOrderFormCard.isChecked
+            val paymentRadio3 = radioButtonOrderFormNaverPay.isChecked
+            val agree1 = checkBoxOrderFormAgree1.isChecked
+            var agree2 = checkBoxOrderFormAgree2.isChecked
 
+            if (ordererName.isEmpty()) {
+                val builder = MaterialAlertDialogBuilder(mainActivity)
+                builder.setTitle("주문자명 오류")
+                builder.setMessage("주문자명을 입력해주세요")
+                builder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                    mainActivity.showSoftInput(editTextOrderFormOrdererName)
+                }
+                builder.show()
+                return
+            }
+
+            if (ordererPhone.isEmpty()) {
+                val builder = MaterialAlertDialogBuilder(mainActivity)
+                builder.setTitle("주문자 연락처 오류")
+                builder.setMessage("주문자 연락처를 입력해주세요")
+                builder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                    mainActivity.showSoftInput(editTextOrderFormOrdererPhone)
+                }
+                builder.show()
+                return
+            }
+
+            if (ordererEmail.isEmpty()) {
+                val builder = MaterialAlertDialogBuilder(mainActivity)
+                builder.setTitle("주문자 이메일 오류")
+                builder.setMessage("주문자 이메일을 입력해주세요")
+                builder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                    mainActivity.showSoftInput(editTextOrderFormOrdererEmail)
+                }
+                builder.show()
+                return
+            }
+
+            if (receiverName.isEmpty()) {
+                val builder = MaterialAlertDialogBuilder(mainActivity)
+                builder.setTitle("배송지 받는사람 오류")
+                builder.setMessage("배송지 받는사람을 입력해주세요")
+                builder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                    mainActivity.showSoftInput(editTextOrderFormReceiverName)
+                }
+                builder.show()
+                return
+            }
+
+            if (addressPhone.isEmpty()) {
+                val builder = MaterialAlertDialogBuilder(mainActivity)
+                builder.setTitle("배송지 연락처 오류")
+                builder.setMessage("배송지 연락처을 입력해주세요")
+                builder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                    mainActivity.showSoftInput(editTextOrderFormAddressPhone)
+                }
+                builder.show()
+                return
+            }
+
+            if (zipCode.isEmpty()) {
+                val builder = MaterialAlertDialogBuilder(mainActivity)
+                builder.setTitle("우편번호 오류")
+                builder.setMessage("우편번호를 입력해주세요")
+                builder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                    mainActivity.showSoftInput(editTextOrderFormZipCode)
+                }
+                builder.show()
+                return
+            }
+
+            if (detailAddress.isEmpty()) {
+                val builder = MaterialAlertDialogBuilder(mainActivity)
+                builder.setTitle("상세주소 오류")
+                builder.setMessage("상세주소를 입력해주세요")
+                builder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                    mainActivity.showSoftInput(editTextOrderFormDetailAddress)
+                }
+                builder.show()
+                return
+            }
+
+            if (paymentMethod != R.id.radioButton_orderForm_deposit && paymentMethod != R.id.radioButton_orderForm_card && paymentMethod != R.id.radioButton_orderForm_naverPay) {
+                val builder = MaterialAlertDialogBuilder(mainActivity)
+                builder.setTitle("결제수단 오류")
+                builder.setMessage("결제수단을 선택해주세요")
+                builder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                    radioGroupOrderFormPayment.requestFocus()
+                }
+                builder.show()
+                return
+            }
+
+            if (agree1 == false || agree2 == false) {
+                val builder = MaterialAlertDialogBuilder(mainActivity)
+                builder.setTitle("필수 동의 오류")
+                builder.setMessage("필수 동의사항에 동의해주세요")
+                builder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                    checkBoxOrderFormAllAgree.requestFocus()
+                }
+                builder.show()
+                return
+            }
+
+            mainActivity.replaceFragment(MainActivity.ORDER_CHECK_FORM_CUSTOMER_FRAGMENT, true, null)
+        }
+    }
 }
