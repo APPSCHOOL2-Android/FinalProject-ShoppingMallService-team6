45be5023ef3a99fe4427be61fce67945cf4de931
dkin12 <dkin1020@naver.com>
2023-08-24 10:24:47 +0900

    [feat] 카카오 로그인 개발하기
    카카오 로그인 개발완료

--- KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/LoginSellerMain/LoginSellerMainFragment.kt
+++ KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/LoginSellerMain/LoginSellerMainFragment.kt
@@ -1,45 +1,57 @@
 package com.test.keepgardeningproject_seller.UI.LoginSellerMain
 
+import android.content.ContentValues
+import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import com.kakao.sdk.user.UserApiClient
+import com.navercorp.nid.NaverIdLoginSDK
+import com.nhn.android.oauth.BuildConfig
 import com.test.keepgardeningproject_seller.API.KakaoAPI
+import com.test.keepgardeningproject_seller.DAO.UserSellerInfo
 import com.test.keepgardeningproject_seller.MainActivity
+import com.test.keepgardeningproject_seller.Repository.UserRepository
+import com.test.keepgardeningproject_seller.Repository.UserRepository.Companion.getUserSellerInfoById
 import com.test.keepgardeningproject_seller.databinding.FragmentLoginSellerMainBinding
+import kotlin.math.log
 
 class LoginSellerMainFragment : Fragment() {
 
     lateinit var fragmentLoginSellerMainBinding: FragmentLoginSellerMainBinding
     lateinit var mainActivity: MainActivity
 
-    lateinit var loginSellerMainViewModel: LoginSellerMainViewModel
-
-    lateinit var loginEmail:String
-    lateinit var loginPw:String
-    lateinit var nickname: String
-    lateinit var loginType : String
-
     // 카카오톡 API불러오기
     val kakaoApi = KakaoAPI()
 
+
+    // val sharedPref = requireContext().getSharedPreferences("myLogin", Context.MODE_PRIVATE)
+    var user_email : String? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        NaverIdLoginSDK.initialize(requireContext(), "el7gHNDTrFweYrwQdjFT", "Kp1DmQI6qv", "킵 가드닝")
         fragmentLoginSellerMainBinding = FragmentLoginSellerMainBinding.inflate(inflater)
         mainActivity = activity as  MainActivity
 
-
+//        val editor = sharedPref.edit()
+//        editor.putString("user_email", user_email)
+//        editor.putBoolean("is_logged_in", true)
+//        editor.apply()
 
         fragmentLoginSellerMainBinding.run {
             // 카카오 로그인
             buttonLoginSellerMainKakaoLogin.setOnClickListener {
-                // 카카오톡 로그인 호출
-                kakaoApi.CheckLogin(requireContext(),mainActivity)
+                kakaoApi.CheckLogin(requireContext())
+                kakaoLogin()
             }
             // 네이버 로그인
             buttonLoginSellerMainNaverLogin.setOnClickListener {
@@ -58,6 +70,74 @@ class LoginSellerMainFragment : Fragment() {
 
 
         return fragmentLoginSellerMainBinding.root
+    }
+    fun kakaoLogin(){
+        // 로그인 정보 받아오기
+
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+            } else if (user != null) {
+                var email = user.kakaoAccount?.email
+                if (email != null) {
+                    checkEmail(email)
+                }
+                else{
+                    // 데이터가 존재하지 않는 경우 새로 로그인
+                    var joinBundle = Bundle()
+                    joinBundle.putString("joinUserEmail",email)
+                    joinBundle.putLong("joinUserType",1)
+                    mainActivity.replaceFragment(MainActivity.JOIN_SELLER_ADD_INFO_FRAGMENT,false,joinBundle)
+                }
+
+
+            }
+        }
+    }
+    fun checkEmail(loginSellerEmail:String){
+        getUserSellerInfoById(loginSellerEmail) {
+            // 가져온 데이터가 없다면
+            if (it.result.exists() == false) {
+                var joinBundle = Bundle()
+                joinBundle.putString("joinUserEmail",loginSellerEmail)
+                joinBundle.putLong("joinUserType",1)
+                mainActivity.replaceFragment(MainActivity.JOIN_SELLER_ADD_INFO_FRAGMENT, false, joinBundle)
+            }
+            // 가져온 데이터가 있다면
+            else {
+                for (c1 in it.result.children) {
+
+                    // 로그인한 사용자 정보를 가져온다.
+                    val userSellerIdx = c1.child("userSellerIdx").value as Long
+                    val userSellerLoginType = c1.child("userSellerLoginType").value as Long
+                    val userSellerEmail = c1.child("userSellerEmail").value as String
+                    val userSellerPw = c1.child("userSellerPw").value as String
+                    val userSellerNickname = c1.child("userSellerNickname").value as String
+                    val userSellerBanner = c1.child("userSellerBanner").value as String
+                    val userSellerPostNumber = c1.child("userSellerPostNumber").value as String
+                    val userSellerPostDetail = c1.child("userSellerPostDetail").value as String
+                    val userSellerStoreInfo = c1.child("userSellerStoreInfo").value as String
+                    val userSellerStoreName = c1.child("userSellerStoreName").value as String
+
+                    mainActivity.loginSellerInfo = UserSellerInfo(
+                        userSellerIdx,
+                        userSellerLoginType,
+                        userSellerEmail,
+                        userSellerPw,
+                        userSellerNickname,
+                        userSellerBanner,
+                        userSellerStoreName,
+                        userSellerStoreInfo,
+                        userSellerPostNumber,
+                        userSellerPostDetail
+                    )
+                    mainActivity.isLogined = true
+
+                    Snackbar.make(fragmentLoginSellerMainBinding.root, "로그인 되었습니다", Snackbar.LENGTH_SHORT).show()
+
+                    mainActivity.replaceFragment(MainActivity.HOME_SELLER_FRAGMENT, false, null)
+                }
+            }
+        }
     }
 
 
