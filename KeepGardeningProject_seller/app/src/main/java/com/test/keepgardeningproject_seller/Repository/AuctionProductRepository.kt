02db322c488f351f517fd0ba29943297253e0e97
PIYUJIN <yjyj020201@gmail.com>
2023-08-24 10:26:48 +0900

    [Feat] AuctionProductRepository.kt 파일 생성 및 기능 구현
    AuctionProductRepository.kt 파일에 firebase 경매 상품 정보 저장 기능 구현

---
+++ KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/Repository/AuctionProductRepository.kt
@@ -0,0 +1,52 @@
+package com.test.keepgardeningproject_seller.Repository
+
+import android.net.Uri
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+import com.test.keepgardeningproject_seller.DAO.AuctionProductClass
+import com.test.keepgardeningproject_seller.DAO.ProductClass
+
+class AuctionProductRepository {
+
+    companion object {
+
+        // 경매 상품 인덱스 가져오기
+        fun getAuctionProductIdx(callback1: (Task<DataSnapshot>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            // 게시글 인덱스 번호
+            val auctionProductIdxRef = database.getReference("AuctionProductIdx")
+            auctionProductIdxRef.get().addOnCompleteListener(callback1)
+        }
+
+        // 경매 상품 인덱스 저장
+        fun setAuctionProductIdx(auctionProductIdx: Long, callback1: (Task<Void>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val auctionProductIdxRef = database.getReference("AuctionProductIdx")
+
+            auctionProductIdxRef.get().addOnCompleteListener {
+                it.result.ref.setValue(auctionProductIdx).addOnCompleteListener(callback1)
+            }
+        }
+
+        // 경매 상품 정보 저장
+        fun addAuctionProductInfo(auctionProductDataClass: AuctionProductClass, callback1: (Task<Void>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val auctionProductIdxRef = database.getReference("AuctionProduct")
+            auctionProductIdxRef.push().setValue(auctionProductDataClass).addOnCompleteListener(callback1)
+        }
+
+        // 경매 상품 이미지 업로드
+        fun uploadImage(
+            uploadUri: Uri,
+            fileName: String,
+            callback1: (Task<UploadTask.TaskSnapshot>) -> Unit
+        ) {
+            val storage = FirebaseStorage.getInstance()
+            val imageRef = storage.reference.child(fileName)
+            imageRef.putFile(uploadUri).addOnCompleteListener(callback1)
+        }
+    }
+}
