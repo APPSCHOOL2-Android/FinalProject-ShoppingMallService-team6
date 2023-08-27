3edb508129912400668419f39e47314b2a752d07
PIYUJIN <yjyj020201@gmail.com>
2023-08-28 04:23:20 +0900

    [Feat] AuctionSellerEditFragment.kt 파일 경매 상품 수정 기능 구현
    AuctionSellerEditFragment.kt 파일에 이미지 업로드 및 경매 상품 수정 기능 구현

--- KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/AuctionSellerEdit/AuctionSellerEditFragment.kt
+++ KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/UI/AuctionSellerEdit/AuctionSellerEditFragment.kt
@@ -114,6 +114,20 @@ class AuctionSellerEditFragment : Fragment() {
                 adapter?.notifyDataSetChanged()
             }
 
+            buttonAuctionSellerEditAddImage.setOnClickListener {
+                if (uriList.count() == MAX_IMAGE_NUM) {
+                    Snackbar.make(fragmentAuctionSellerEditBinding.root, "이미지는 최대 ${MAX_IMAGE_NUM}장까지 첨부할 수 있습니다.", Snackbar.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.type = "image/*"
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+                registerForActivityResult.launch(intent)
+
+                var adapter = fragmentAuctionSellerEditBinding.recyclerViewAuctionSellerEditImage.adapter as RecyclerAdapterClass
+                adapter.notifyDataSetChanged()
+            }
+
             buttonAuctionSellerEditEdit.setOnClickListener {
 
                 var auctionProductName = textInputEditTextAuctionSellerEditProductName.text.toString()
@@ -143,6 +157,46 @@ class AuctionSellerEditFragment : Fragment() {
                     return@setOnClickListener
                 }
 
+                Snackbar.make(fragmentAuctionSellerEditBinding.root, "경매 정보가 수정되었습니다.", Snackbar.LENGTH_SHORT).show()
+
+
+                for (i in 0 until uriList.count()) {
+                    val fileName = if(i< originImageNum) {
+                        fileNameList[i]
+                    } else {
+                        "image/img_${System.currentTimeMillis()}_$i.jpg"
+                    }
+
+                    imageList.add(fileName)
+                }
+
+                var auctionProductOpenPrice = arguments?.getString("auctionProductOpenPrice").toString()
+                var auctionProductOpenDate = arguments?.getString("auctionProductOpenDate").toString()
+                var auctionProductCloseDate = arguments?.getString("auctionProductCloseDate").toString()
+
+                val auctionProductDataClass = AuctionProductClass(
+                    auctionProductIdx.toLong(),
+                    imageList,
+                    auctionProductName,
+                    auctionProductOpenPrice,
+                    mainActivity.loginSellerInfo.userSellerIdx,
+                    auctionProductOpenDate,
+                    auctionProductCloseDate,
+                    auctionProductContent
+                )
+
+                // 경매 상품 정보 저장
+                AuctionProductRepository.modifyAuctionProduct(auctionProductDataClass) {
+
+                }
+                for (i in 0 until uriList.count()) {
+                    // 이미지 업로드
+                    ProductRepository.uploadImage(uriList[i]!!, imageList[i]) {
+
+                    }
+                }
+
+                SystemClock.sleep(10000)
                 mainActivity.removeFragment(AUCTION_SELLER_EDIT_FRAGMENT)
             }
         }
@@ -154,6 +208,34 @@ class AuctionSellerEditFragment : Fragment() {
         var adapter = fragmentAuctionSellerEditBinding.recyclerViewAuctionSellerEditImage.adapter as RecyclerAdapterClass
         adapter.notifyDataSetChanged()
     }
+
+    private val registerForActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            when (result.resultCode) {
+                AppCompatActivity.RESULT_OK -> {
+                    val clipData = result.data?.clipData
+                    if (clipData != null) { // 이미지를 여러 개 선택할 경우
+                        val clipDataSize = clipData.itemCount
+                        val selectableCount = MAX_IMAGE_NUM - uriList.count()
+                        if (clipDataSize > selectableCount) { // 최대 선택 가능한 개수를 초과해서 선택한 경우
+                            Snackbar.make(fragmentAuctionSellerEditBinding.root, "이미지는 최대 ${MAX_IMAGE_NUM}장까지 첨부할 수 있습니다.", Snackbar.LENGTH_SHORT).show()
+                        } else {
+                            // 선택 가능한 경우 ArrayList에 가져온 uri를 넣어준다.
+                            for (i in 0 until clipDataSize) {
+                                uriList.add(clipData.getItemAt(i).uri)
+                            }
+                        }
+                    } else { // 이미지를 한 개만 선택할 경우 null이 올 수 있다.
+                        val uri = result?.data?.data
+                        if (uri != null) {
+                            uriList.add(uri)
+                        }
+                    }
+                    var adapter = fragmentAuctionSellerEditBinding.recyclerViewAuctionSellerEditImage.adapter as RecyclerAdapterClass
+                    adapter.notifyDataSetChanged()
+                }
+            }
+        }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
