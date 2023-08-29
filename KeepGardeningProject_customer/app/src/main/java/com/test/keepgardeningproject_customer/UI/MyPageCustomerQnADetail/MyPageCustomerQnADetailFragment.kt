5a6be8a5702573588dde842481603a540c2f9097
PIYUJIN <yjyj020201@gmail.com>
2023-08-29 10:03:11 +0900

    [Feat] MyPageCustomerQnADetailFragment.kt 파일 기능 구현
    MyPageCustomerQnADetailFragment.kt 파일에 viewModel 연결

--- KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/MyPageCustomerQnADetail/MyPageCustomerQnADetailFragment.kt
+++ KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/MyPageCustomerQnADetail/MyPageCustomerQnADetailFragment.kt
@@ -1,8 +1,13 @@
 package com.test.keepgardeningproject_customer.UI.MyPageCustomerQnADetail
 
+import android.content.res.Resources
+import android.graphics.Color
+import android.net.Uri
 import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +16,7 @@ import androidx.annotation.RequiresApi
 import com.test.keepgardeningproject_customer.MainActivity
 import com.test.keepgardeningproject_customer.R
 import com.test.keepgardeningproject_customer.databinding.FragmentMyPageCustomerQnADetailBinding
-import java.time.LocalDate
+
 
 class MyPageCustomerQnADetailFragment : Fragment() {
 
@@ -21,9 +26,12 @@ class MyPageCustomerQnADetailFragment : Fragment() {
 
     private lateinit var viewModel: MyPageCustomerQnADetailViewModel
 
-    lateinit var binding:FragmentMyPageCustomerQnADetailBinding
-
+    lateinit var binding: FragmentMyPageCustomerQnADetailBinding
     lateinit var mainActivity: MainActivity
+
+    var qnaIdx:Long = 0
+    var uriList = mutableListOf<Uri>()
+    private val handler = Handler()
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
@@ -32,12 +40,54 @@ class MyPageCustomerQnADetailFragment : Fragment() {
     ): View? {
 
         binding = FragmentMyPageCustomerQnADetailBinding.inflate(inflater)
-
         mainActivity = activity as MainActivity
 
-        val view = binding.root
+        qnaIdx = arguments?.getLong("qnaIdx", 0)!!
 
-        binding.run{
+        viewModel = ViewModelProvider(mainActivity)[MyPageCustomerQnADetailViewModel::class.java]
+        viewModel.run {
+
+            qnaTitle.observe(mainActivity) {
+                binding.editTextViewQcDetailTitle.setText(it)
+            }
+            qnaContent.observe(mainActivity) {
+                binding.editTextViewQcDetailContent.setText(it)
+            }
+            qnaDate.observe(mainActivity) {
+                binding.textviewQcDate.text = it
+            }
+            qnaAnswer.observe(mainActivity) {
+                if(it == "None") {
+                    binding.textviewQcDetailReplyState.run {
+                        text = "미답변"
+                        setTextColor(resources.getColor(R.color.Red))
+                    }
+                    binding.editTextViewQcDetailAnswer.visibility = View.GONE
+                }
+                else {
+                    binding.textviewQcDetailReplyState.run {
+                        text = "답변 완료"
+                        setTextColor(resources.getColor(R.color.colorAccent3))
+                    }
+                    binding.editTextViewQcDetailAnswer.run {
+                        visibility = View.VISIBLE
+                        setText(it)
+                    }
+                }
+            }
+            qnaImageNameList.observe(mainActivity) {
+//                binding.recyclerViewQcImage.adapter?.notifyDataSetChanged()
+            }
+            qnaImageUriList.observe(mainActivity) {
+                uriList = it
+                Log.d("lion","uri : $uriList")
+//                binding.recyclerViewQcImage.adapter?.notifyDataSetChanged()
+            }
+            viewModel.getQnAInfo(qnaIdx)
+        }
+
+
+        binding.run {
 
             materialToolbarQcDetail.run{
 
@@ -60,25 +110,28 @@ class MyPageCustomerQnADetailFragment : Fragment() {
 
             }
 
-            val now = LocalDate.now()
-
-            textviewQcCurrentTime.text = now.toString()
-
-            val imageId = arguments?.getInt("contentImage")
-
-            imageId?.let{
-
-                imageviewQcDetail.setImageResource(it)
-
+            editTextViewQcDetailTitle.run {
+                isEnabled = false
+                setTextColor(Color.BLACK)
+            }
+            editTextViewQcDetailContent.run {
+                isEnabled = false
+                setTextColor(Color.BLACK)
+            }
+            editTextViewQcDetailAnswer.run {
+                isEnabled = false
+                setTextColor(Color.BLACK)
             }
 
-            editTextViewQcDetailTitle.hint = arguments?.getString("contentTitle")
 
-            editTextViewQcDetailContent.hint = arguments?.getString("contentQnA")
+//            recyclerViewQcImage.run {
+//                adapter = RecyclerViewAdapter()
+//                layoutManager = CarouselLayoutManager()
+//            }
 
         }
 
-        return view
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -86,5 +139,40 @@ class MyPageCustomerQnADetailFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(MyPageCustomerQnADetailViewModel::class.java)
         // TODO: Use the ViewModel
     }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getQnAInfo(qnaIdx)
+//        binding.recyclerViewQcImage.adapter?.notifyDataSetChanged()
+    }
+
+//    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolderClass>(){
+//        inner class ViewHolderClass(rowBinding: RowQnaImageBinding) : RecyclerView.ViewHolder(rowBinding.root){
+//
+//            var carouselImageView : ImageView
+//
+//            init {
+//                carouselImageView = rowBinding.carouselImageViewRowQnAImage
+//            }
+//
+//        }
+//
+//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+//            val rowBinding = RowQnaImageBinding.inflate(layoutInflater)
+//            val viewHolderClass = ViewHolderClass(rowBinding)
+//
+//            return viewHolderClass
+//        }
+//
+//        override fun getItemCount(): Int {
+//            Log.d("lion","size : ${uriList.size}")
+//            return uriList.size
+//        }
+//
+//        override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+////            holder.carouselImageView.setImageURI(uriList[position])
+//            Glide.with(mainActivity).load(uriList[position]).into(holder.carouselImageView)
+//        }
+//    }
 
 }
