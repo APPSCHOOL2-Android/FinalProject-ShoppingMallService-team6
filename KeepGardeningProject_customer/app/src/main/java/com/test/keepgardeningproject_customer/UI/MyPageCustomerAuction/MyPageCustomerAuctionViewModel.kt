2366754d161c4b0861f073d2462df49a5609b28d
Seong Wook Lim <40172001+SeongWk@users.noreply.github.com>
2023-08-29 17:01:12 +0900

    [feat]구매자 구매내역 및 경매내역 재수정 및  DAO 수정

--- KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/MyPageCustomerAuction/MyPageCustomerAuctionViewModel.kt
+++ KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/MyPageCustomerAuction/MyPageCustomerAuctionViewModel.kt
@@ -3,6 +3,8 @@ package com.test.keepgardeningproject_customer.UI.MyPageCustomerAuction
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.test.keepgardeningproject_customer.DAO.auctionCustomerDetail
+import com.test.keepgardeningproject_customer.DAO.purchaseInfo
 import com.test.keepgardeningproject_customer.MainActivity
 import com.test.keepgardeningproject_customer.Repository.AuctionProductRepository
 import com.test.keepgardeningproject_customer.Repository.AuctionRepository
@@ -11,42 +13,25 @@ import java.util.Calendar
 import java.util.Date
 
 class MyPageCustomerAuctionViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
 
     var getauctiondetailList = MutableLiveData<MutableList<auctionCustomerDetail>>()
 
     init {
         getauctiondetailList.value = mutableListOf<auctionCustomerDetail>()
     }
-    fun getData(){
 
+    fun resetList(){
+        getauctiondetailList.value  = mutableListOf<auctionCustomerDetail>()
+    }
+    fun getAll(){
 
-        //이미지,상태,이름
-
-        //내가고른 경매상품의 인덱스
-
-        //AuctionDA0 - auctionCustomerList(useridx)
-
-        //AuctionProduct
-        // -> auctionProductName(이름),auctionImageList(이미지),auctionCloseState(상태),
-
-
-
-        //userSellerIdx = auctionProductstoreIdx(판매자 idx)
-
-        //useridx == auctioncustomeridx
-        //auctionAuctionProductIndex = auctionProductIdx
         var idx = MainActivity.loginedUserInfo.userIdx
-
-        //useridx가져옴
-        Log.d("Lim useridx","${idx}")
-
         var templist = mutableListOf<auctionCustomerDetail>()
 
-        //내가 입찰한 상품들을 가져와야 함 - 경매중과 경매종료를 나누기 위해
         AuctionRepository.getAuctionInfoByUserIdx(idx!!){
             for(c1 in it.result.children){
                 var auctionproductidx = c1.child("auctionAuctionProductIndex").value as Long
+                var auctionbidPrice =c1.child("auctionBidPrice").value as String
                 AuctionProductRepository.getAuctionProductInfo(auctionproductidx){
                     for(c1 in it.result.children){
                         var name = c1.child("auctionProductName").value as String
@@ -54,8 +39,9 @@ class MyPageCustomerAuctionViewModel : ViewModel() {
                         var newimg = img[0]
                         var state = c1.child("auctionProductCloseDate").value as String
                         var newstate = getTime(state)
+                        var productidx = c1.child("auctionProductIdx").value as Long
 
-                        var newclass = auctionCustomerDetail(name,newimg,newstate)
+                        var newclass = auctionCustomerDetail(name,newimg,newstate,auctionbidPrice,productidx)
                         Log.d("Lim class","${newclass}")
 
                         templist.add(newclass)
@@ -63,6 +49,8 @@ class MyPageCustomerAuctionViewModel : ViewModel() {
                     }
                 }
             }
+
+
         }
 
     }
