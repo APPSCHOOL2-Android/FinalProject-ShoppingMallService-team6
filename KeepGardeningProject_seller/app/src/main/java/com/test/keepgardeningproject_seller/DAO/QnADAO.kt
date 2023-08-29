4e38d852188e513111cc6a6bc8e9a17c3e8dfc2b
PIYUJIN <yjyj020201@gmail.com>
2023-08-30 02:14:48 +0900

    [Feat] QnADAO.kt 파일 생성 및 data class 구현
    QnADAO.kt 파일에 문의 내역 QnAClass 구성

---
+++ KeepGardeningProject_seller/app/src/main/java/com/test/keepgardeningproject_seller/DAO/QnADAO.kt
@@ -0,0 +1,16 @@
+package com.test.keepgardeningproject_seller.DAO
+
+class QnADAO {
+}
+
+data class QnAClass(
+    var QnAIdx: Long,
+    var QnAProductType: String,
+    var QnAProductIdx: Long,
+    var QnACustomerIdx: Long,
+    var QnAStoreIdx: Long,
+    var QnATitle: String,
+    var QnAContent: String,
+    var QnAAnswer: String,
+    var QnADate: String
+)
