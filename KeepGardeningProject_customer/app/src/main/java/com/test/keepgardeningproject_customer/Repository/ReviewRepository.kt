47e723a7dcc2273174bc284668aec2e63aff87aa
ShShin98 <hss980903@gmail.com>
2023-08-30 14:31:50 +0900

    [Feat] 마이페이지 리뷰 작성 기능 추가

--- KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/Repository/ReviewRepository.kt
+++ KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/Repository/ReviewRepository.kt
@@ -9,57 +9,62 @@ import com.test.keepgardeningproject_customer.DAO.MypageReview
 import com.test.keepgardeningproject_customer.DAO.Review
 
 class ReviewRepository {
-
-    companion object{
-
-        //제품 이름 얻어오기
-
-        fun getProductNameByProductIdx(productIdx:Long,callback1:(Task<DataSnapshot>)->Unit){
-
+    companion object {
+        // 리뷰 idx 가져오기
+        fun getReviewIdx(callback1: (Task<DataSnapshot>) -> Unit) {
             val database = FirebaseDatabase.getInstance()
+            val reviewIdxRef = database.getReference("ReviewIdx")
 
+            reviewIdxRef.get().addOnCompleteListener(callback1)
+        }
+
+        // 리뷰 idx 저장하기
+        fun setReviewIdx(reviewIdx: Long, callback1: (Task<Void>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val reviewIdxRef = database.getReference("ReviewIdx")
+
+            reviewIdxRef.get().addOnCompleteListener {
+                it.result.ref.setValue(reviewIdx).addOnCompleteListener(callback1)
+            }
+        }
+
+        // 리뷰 데이터 저장하기
+        fun addReviewInfo(review: Review, callback1: (Task<Void>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val reviewDataRef = database.getReference("Review")
+
+            reviewDataRef.push().setValue(review).addOnCompleteListener(callback1)
+        }
+
+        // 상품 정보 얻어오기
+        fun getProductByProductIdx(productIdx: Long, callback1: (Task<DataSnapshot>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
             val productRef = database.getReference("Product")
 
             productRef.orderByChild("productIdx").equalTo(productIdx.toDouble()).get().addOnCompleteListener(callback1)
-
         }
 
-        //상점 이름 얻어오기
-        fun getStoreNameByStoreIdx(target_Idx: Long,callback1:(Task<DataSnapshot>)->Unit){
-
+        // 상점 정보 얻어오기
+        fun getStoreByStoreIdx(storeIdx: Long, callback1: (Task<DataSnapshot>) -> Unit) {
             val database = FirebaseDatabase.getInstance()
-
             val storeRef = database.getReference("UserSellerInfo")
 
-            storeRef.orderByChild("userSellerIdx").equalTo(target_Idx.toDouble()).get().addOnCompleteListener(callback1)
-
+            storeRef.orderByChild("userSellerIdx").equalTo(storeIdx.toDouble()).get().addOnCompleteListener(callback1)
         }
 
 
-        //리뷰 데이터베이스에 올리기
-        fun uploadReview(userIdx:String,review: Review){
+        fun getReviewByuserIdx(userIdx: String, callback1: (Task<DataSnapshot>) -> Unit) {
 
             val database = FirebaseDatabase.getInstance()
 
             val reviewRef = database.getReference("Review")
 
-            reviewRef.child(userIdx).push().setValue(review)
+            reviewRef.orderByChild("userIdx").equalTo(userIdx).get()
+                .addOnCompleteListener(callback1)
 
         }
 
-        //Review->user ->review1,review2,review3....이다. review들을 얻어오는 함수이다.
-
-        fun getReviewByuserIdx(userIdx:String,callback1: (Task<DataSnapshot>) -> Unit){
-
-            val database = FirebaseDatabase.getInstance()
-
-            val reviewRef = database.getReference("Review")
-
-            reviewRef.orderByChild("userIdx").equalTo(userIdx).get().addOnCompleteListener(callback1)
-
-        }
-
-        fun getUserReview(userIdx:String,callback1: (Task<DataSnapshot>) -> Unit){
+        fun getUserReview(userIdx: String, callback1: (Task<DataSnapshot>) -> Unit) {
 
             val database = FirebaseDatabase.getInstance()
 
@@ -69,7 +74,6 @@ class ReviewRepository {
 
 
         }
-
 
 
     }
