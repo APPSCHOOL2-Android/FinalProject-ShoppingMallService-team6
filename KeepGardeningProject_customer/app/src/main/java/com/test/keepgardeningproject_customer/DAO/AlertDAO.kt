2ac00ededd1e6f388e27eea0ac1d0183e32959a7
dkin12 <dkin1020@naver.com>
2023-08-30 09:01:49 +0900

    [feat] alert DAO추가

---
+++ KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/DAO/AlertDAO.kt
@@ -0,0 +1,12 @@
+package com.test.keepgardeningproject_customer.DAO
+
+
+class AlertDAO(){
+    
+}
+    
+data class Alert(
+    var alertIdx:Long,
+    var alertUserIndex : Long,
+    var alertContent : String
+)
