839bca0abb9fcde8409d46fb5820a1e26cd5a960
ShShin98 <hss980903@gmail.com>
2023-08-31 10:06:49 +0900

    [Feat] 마이페이지 리뷰 내역에서 리뷰 상세정보 보여주는 기능 추가

--- KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/Repository/ReviewRepository.kt
+++ KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/Repository/ReviewRepository.kt
@@ -1,10 +1,12 @@
 package com.test.keepgardeningproject_customer.Repository
 
+import android.net.Uri
 import com.google.android.gms.tasks.Task
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 import com.test.keepgardeningproject_customer.DAO.MypageReview
 import com.test.keepgardeningproject_customer.DAO.Review
 
@@ -37,6 +39,26 @@ class ReviewRepository {
             reviewDataRef.push().setValue(review).addOnCompleteListener(callback1)
         }
 
+        // 리뷰 데이터 삭제하기
+        fun removeReviewInfo(reviewIdx: Long, callback1: (Task<Void>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val reviewDataRef = database.getReference("Review")
+
+            reviewDataRef.orderByChild("reviewIdx").equalTo(reviewIdx.toDouble()).get().addOnCompleteListener {
+                for (a1 in it.result.children) {
+                    // 해당 데이터를 삭제한다.
+                    a1.ref.removeValue().addOnCompleteListener(callback1)
+                }
+            }
+        }
+
+        // 리뷰 idx를 통해 리뷰 데이터 얻어오기
+        fun getReviewByIdx(reviewIdx: Long, callback1: (Task<DataSnapshot>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val reviewDataRef = database.getReference("Review")
+
+            reviewDataRef.orderByChild("reviewIdx").equalTo(reviewIdx.toDouble()).get().addOnCompleteListener(callback1)
+        }
 
         // 상점 정보 얻어오기
         fun getStoreByStoreIdx(storeIdx: Long, callback1: (Task<DataSnapshot>) -> Unit) {
@@ -57,16 +79,20 @@ class ReviewRepository {
 
 
 
-
-        fun getReviewByuserIdx(userIdx: String, callback1: (Task<DataSnapshot>) -> Unit) {
-
+        // 사용자 idx를 통해 리뷰 불러오기
+        fun getReviewByUserIdx(userIdx: String, callback1: (Task<DataSnapshot>) -> Unit) {
             val database = FirebaseDatabase.getInstance()
-
             val reviewRef = database.getReference("Review")
 
-            reviewRef.orderByChild("userIdx").equalTo(userIdx).get()
-                .addOnCompleteListener(callback1)
+            reviewRef.orderByChild("userIdx").equalTo(userIdx).get().addOnCompleteListener(callback1)
+        }
 
+        // 상품 이미지 불러오기
+        fun getProductImage(fileName : String, callback1:(Task<Uri>) -> Unit){
+            val storage = FirebaseStorage.getInstance()
+            val fileRef = storage.reference.child(fileName)
+
+            fileRef.downloadUrl.addOnCompleteListener(callback1)
         }
 
         fun getUserReview(userIdx: String, callback1: (Task<DataSnapshot>) -> Unit) {
