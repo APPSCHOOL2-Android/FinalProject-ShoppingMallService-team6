95b7748b9d98f5c15f6cbc90d9b166458eccfeca
JUNGCHAEYOON <108243290+JUNGCHAEYOON@users.noreply.github.com>
2023-08-31 16:40:00 +0900

    [Fix] 리뷰
    튕기는거 해결

--- KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/MyPageCustomerReview/MyPageCustomerReviewFragment.kt
+++ KeepGardeningProject_customer/app/src/main/java/com/test/keepgardeningproject_customer/UI/MyPageCustomerReview/MyPageCustomerReviewFragment.kt
@@ -55,7 +55,7 @@ class MyPageCustomerReviewFragment : Fragment() {
 
             recyclerViewMyPageCustomerReview.run {
                 adapter = ReviewRecyclerViewAdapter()
-                layoutManager = LinearLayoutManager(context)
+                layoutManager = LinearLayoutManager(mainActivity)
             }
 
             viewModel.getReviewData(userIdx.toString())
